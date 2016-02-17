67d2df2024d0c520a700ddaeb9f1ceefaa69528a
EdDewey <ed.dewey@gmail.com>
2016-02-17 15:07:20 -0600

    Update sl2.py
    Decomposed the Weil representation into its two irreducible sub-representations

--- sl2.py
+++ sl2.py
@@ -5,16 +5,19 @@
 
 
 # work with the field with p elements, modelled as Z/p
-p = 3
+p = 5
 q = p # code for psi assumes q=p!
 F = GF(p, repr='int')
 FMat = MatrixSpace(F, 2, 2)
-CMat = MatrixSpace(CC, q, q)
+CMat = MatrixSpace(CC, q, q) 
+CMatEven = MatrixSpace(CC, floor(p/2)+1, floor(p/2)+1)
+CMatOdd = MatrixSpace(CC, floor(p/2), floor(p/2))
 gen1 = FMat(matrix([[0, -1], [1, 0]]))
 gen2 = FMat(matrix([[1,  1], [0, 1]]))
 
 
 # fix an additive character psi.   
+# When we upgrade to prime powers, we will need to precompose with a trace function.
 def psi(a):
 	return exp(sage.rings.integer.Integer(a)*2*I*pi/q)
 
@@ -33,13 +36,16 @@ def rhoW():
 			m[i,j] = psi(-i*j)
 	return I**((p-1)/2)/sqrt(q)*m
 
+# Let's precompute
+WW = rhoW()
+
 # output rho(z_a) in the delta function basis
 def rhoZ(a):
 	m = matrix(q,q,I-I) # initialize zero matrix
 	for i in range(0,q):
 		j = a*F(i)    # "F(i)" means i is interpreted in F
 		m[i,j] = 1    # m is now the permutation matrix for f(x) -> f(a^(-1)x) in terms of delta functions
-	c = kronecker(a,q)
+	c = kronecker(a,q)    # kronecker is Legendre symbol
 	m = c*m  
 	return m
 
@@ -49,11 +55,10 @@ def rho(m):
 	b = m[0,1]
 	c = m[1,0]
 	d = m[1,1]
-	rw = rhoW()
-	if a == 0:
-		output = rhoZ(b)*rw*rhoN(-b*d)
+	if c == 0:
+		output = rhoZ(a)*rhoN(b/a)
 	else:
-		output = rhoZ(a)*rw.inverse()*rhoN(-a*c)*rw*rhoN(b/a)
+		output = rhoN(a/c)*WW*rhoZ(-c)*rhoN(d/c)
 	return output
 
 # create a random matrix for testing, in the stupidest possible way
@@ -63,21 +68,24 @@ def randSL2():
 		if m.determinant() == 1:
 			return m
 
-# Outputs rho(A)rho(B) - rho(A*B) coerced to complex entries
-def homoTest(A, B):
-	rhoA = CMat(rho(A))
-	rhoB = CMat(rho(B))
-	rhoAB = CMat(rho(A*B))
-	return rhoA*rhoB - rhoAB
+# Outputs f(A)f(B) - f(A*B) coerced to complex entries.
+# f is a function to GLn
+def homoTest(A, B, f, n):
+	CMatn = MatrixSpace(CC, n, n)
+	fA = CMatn(f(A))
+	fB = CMatn(f(B))
+	fAB = CMatn(f(A*B))
+	return fA*fB - fAB
 
-# Test n times, output the worst result
-def manyHomoTest(blah):
+# Test the purported homomorpism f: SL2 --> GLn, blah times, and output the worst result
+# If you get two zero matrices, you win.  
+def manyHomoTest(blah, f, n):
 	exampleA = matrix(F, q,q,0)
 	exampleB = matrix(F, q,q,0)
 	for i in range(0,blah):
 		tempA = randSL2()
 		tempB = randSL2()
-		diff = homoTest(tempA, tempB)
+		diff = homoTest(tempA, tempB, f, n)
 		maxDiff = max(max(max(diff)))
 		if maxDiff > 10**(-15):
 			exampleA = tempA
@@ -85,6 +93,47 @@ def manyHomoTest(blah):
 			break
 	return [exampleA, exampleB]
 
-# evaluate the rum of rho(g) as g runs over the two generators and their inverses
-def genSum():
-	return rho(gen1) + rho(gen2) + rho(gen1.inverse()) + rho(gen2.inverse())
+# evaluate the sum of f(g) as g runs over the two generators and their inverses
+def genSum(f):
+	return f(gen1) + f(gen2) + f(gen1.inverse()) + f(gen2.inverse())
+
+################################################################################
+# In what follows we want to restrict the Weil representation to the subspaces #
+# of odd and even functions.  Our basis for the even functions consists of     #
+# d_a + d_{p-a}, where d_a is the delta function at a, as a runs from 0 up to  #
+# floor(p/2).  Our basis for odd function is d_a - d_{p-a} as a runs from 1 up #
+# to floor(p/2).  This will need to be reworked for the prime power case.      #
+################################################################################
+
+
+# Operator projecting onto the even functions by f --> [f(x) + f(-x)]/2
+def evenProjector():
+	pHalf = floor(p/2)
+	output = matrix(QQ, pHalf+1, q, 0)
+	output[0,0]=1
+	for i in range(1,pHalf+1):
+		output[i,i]=0.5
+		output[i,q-i]=0.5
+	return output
+
+# Operator projecting onto the odd functions by f --> [f(x)-f(-x)]/2
+def oddProjector():
+	pHalf = floor(p/2)
+	output = matrix(QQ, pHalf, q, 0)
+	for i in range(0, pHalf):
+		output[i,i+1]=0.5
+		output[i,q-i-1]=-0.5
+	return output
+
+# sections of evenProjector and oddProjector
+es = 2*ep.transpose()
+es[0,0]=1
+os = 2*op.transpose()
+
+def rhoEven(m):
+	return ep*rho(m)*es
+
+def rhoOdd(m):
+	return op*rho(m)*os
+	
+
